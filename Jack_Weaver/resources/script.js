1f0742067091a724be01206d6582a70ecda5e5d4
Jack Weaver <weaverjackop@gmail.com>
2022-07-28 18:14:34 -0300

    New Styles

--- Jack_Weaver/resources/script.js
+++ Jack_Weaver/resources/script.js
@@ -2,21 +2,15 @@
 var darkMode =false
 
 function darkSide(){
-    document.getElementById('styles').href='./resources/darkStyles.css'
-    /*let all = document.getElementById("all");
-    all.style.backgroundColor="black";
-    all.style.color="red";
-    document.querySelector("h1").style.color="darkred";
-    document.getElementById("imageOne").src ="C:\Users\jack\OneDrive\Desktop\Jack\Programming\Codecademy\Frontend_Projects\Jack_Weaver\resources\images\Darkside_banner.png";
-    */
+    document.getElementById('styles').href='./resources/darkStyles.css';
+    document.getElementById('imageOne').src='./resources/images/Darkside_banner.png';
+    document.getElementById('imageTwo').src='./resources/images/Darkside_banner.png';
+
+    
 }
 function lightSide(){
     document.getElementById('styles').href='./resources/styles.css'
-    /*let all = document.getElementById("all");
-    all.style.backgroundColor="#deb887";
-    all.style.color= "darkslategray";
-    document.querySelector("h1").style.color="white"
-    */
+    document.getElementById('imageOne').src='./resources/images/Untitled design (21).png';
 }
 document.querySelector("button").onclick = function(){
 if (darkMode === false){
